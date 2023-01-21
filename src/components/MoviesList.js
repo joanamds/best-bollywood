dd4f8e1bb03932b4e82795c7de9183fdb90b7cf8
Joana <joanamdsantos1@gmail.com>
2023-01-21 12:21:36 -0300

    finalizando o input de search

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,7 +1,6 @@
 import React from 'react'; 
 import movies from '../data/Movies';
 import MovieCard from './MovieCard';
-import { FaSearch } from "react-icons/fa";
 class MoviesList extends React.Component{
   constructor(){
     super(); 
@@ -26,13 +25,24 @@ class MoviesList extends React.Component{
     return(
       <div>
         <div className="grid place-items-center bg-orange-500 p-5">
-        <input
-            type="text"
-            placeHolder="Pesquise aqui"
-            onChange={this.handleFilter}
-            className="p-2 w-100 rounded-lg border-4 border-slate-50 outline-none focus:border-blue-500 focus:rounded-lg"
-          />
-          <FaSearch className="absolute inset-y-44 text-xl text-slate-100"/>
+          {/* input font: https://tailwindcomponents.com/component/search-input-with-integrated-icon-and-button */}
+            <form method="GET">
+              <div className="relative text-gray-600 focus-within:text-gray-400">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                  <button type="submit" class="p-1 focus:outline-none focus:shadow-outline">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                  </button>
+                </span>
+              <input
+                type="search"
+                name="q"
+                className="w-100 py-2 text-sm text-white rounded-md pl-10 outline-none focus:text-gray-900"
+                placeholder="Procure aqui..."
+                autocomplete="off"
+                onChange={ this.handleFilter }
+              />
+              </div>
+            </form>
       <h4 className="text-center"> O termo pesquisado é: { filterName } </h4>
       </div>
       <div className="movies-list">
