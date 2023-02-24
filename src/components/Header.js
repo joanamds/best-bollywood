9ee2006c236f6a39b10bedddd8c653a5ad21c00a
Joana <joanamdsantos1@gmail.com>
2023-02-24 10:49:45 -0300

    modificando o nome da aplicação

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends React.Component{
     render() {
         return (
             <header className="text-center bg-white">
-                <h1 className="text-2xl bg-green-600 p-5 font-custom text-white text-5xl">BOLLYWOOD</h1>
+                <h1 className="text-2xl bg-green-600 p-5 font-custom text-white text-5xl">INDIAN MOVIES</h1>
                 <h2 className="p-5">	&#127775;Melhores filmes	&#127775;</h2>
             </header>
         );
